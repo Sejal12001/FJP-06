5d611ec9016e69b6292c5cdaf3d224ad2e4297c2
Sejal12001 <100862687+Sejal12001@users.noreply.github.com>
2022-03-05 23:01:14 +0530

    Update intro.js
    removed comment out

--- intro.js
+++ intro.js
@@ -1,39 +1,39 @@
-// console.log("Swagat hai ap sab ka!!!")
-// console.log("Pepcoders")
+console.log("Swagat hai ap sab ka!!!")
+console.log("Pepcoders")
 
-// //variables in js and data types
+//variables in js and data types
 
-// let num = 10;
-// console.log(num);
+let num = 10;
+console.log(num);
 
-// let char = 'a';
-// console.log(char);
+let char = 'a';
+console.log(char);
 
-// let str = 'I am a coder';
-// console.log(str);
+let str = 'I am a coder';
+console.log(str);
 
-// let bool = true;
-// console.log(bool);
+let bool = true;
+console.log(bool);
 
-// // for(i=1;i<=5; i++)
-// // {
-// //     console.log(i);
-// // }
+for(i=1;i<=5; i++)
+{
+    console.log(i);
+}
 
-// let count = 10;
-// while(count>0)
-// {
-//     console.log(count);
-//     count--;
-// }
+let count = 10;
+while(count>0)
+{
+    console.log(count);
+    count--;
+}
 
-// //is prime
+//is prime
 
-// let p = 7;
-// for(i=2; i<7/2; i++)
-// {
+let p = 7;
+for(i=2; i<7/2; i++)
+{
     
-// }
+}
 var n=  11767
 let isPrime=true
 for(var i =2;i<=n/2;i++){
